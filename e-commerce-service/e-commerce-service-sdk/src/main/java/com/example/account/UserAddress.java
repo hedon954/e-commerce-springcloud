8343851e40617d47c794ef01e29e090311593c5e
hedon954 <171725713@qq.com>
2022-05-31 16:38:37 +0800

    update: init e-commerce-service-sdk module

---
+++ e-commerce-service/e-commerce-service-sdk/src/main/java/com/example/account/UserAddress.java
@@ -0,0 +1,33 @@
+package com.example.account;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Hedon Wang
+ * @create 2022-05-31 4:29 PM
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ApiModel(description = "用户地址基础信息")
+public class UserAddress {
+
+    @ApiModelProperty(value = "用户姓名")
+    private String username;
+
+    @ApiModelProperty(value = "电话")
+    private String phone;
+
+    @ApiModelProperty(value = "省")
+    private String province;
+
+    @ApiModelProperty(value = "市")
+    private String city;
+
+    @ApiModelProperty(value = "详细地址")
+    private String addressDetail;
+}
---
+++ e-commerce-service/e-commerce-service-sdk/src/main/java/com/example/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Hedon Wang
+ * @create 2022-05-31 4:23 PM
+ */
+package com.example;
